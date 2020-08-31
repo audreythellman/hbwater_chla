f8fcb2b09c82009593198a81ef98af58390e35ce
Audrey Thellman <audrey.thellman@duke.edu>
2020-08-31 19:17:51 -0400

    This commit adds a new vingette and updates the code

--- rfu-to-mgm2-chla.R
+++ rfu-to-mgm2-chla.R
@@ -18,35 +18,35 @@ library(lubridate)
 
 #input: rfu, run, subscode, run_error, vol_true (either NA or #s from a column that contains real volumes ), 
 
-rfu_mgm2_chla <- function(rfu, run, sc, run_error, vol_true){
+rfu_mgm2_chla <- function(chla_df, sa_df, slp, blank_df){
   
-  ## load dependencies 
-  savedir <- "C:/Users/Thell/Documents/Duke University/Research/HBEF/FieldExperiments/Summer2019/HB_chla_summer2019/data/tidy/"
-  blanks <- readRDS(paste0(savedir,"blanks_v1.rds"))
-  masterslope <- readRDS(paste0(savedir, "masterslope.rds"))
-  volcalc <- readRDS(paste0(savedir, "volcalc.rds"))
-  surfaceareas <- readRDS(paste0(savedir, "surfaceareas.rds"))
+  ## load dependencies
+  # chla_df <- chla_data
+  # sa_df <- surfaceareas
+  # slp <- slope
+  # blank_df <- blanks_df
   
-  
-  
-      sa <- surfaceareas$surface_area_m2[which(surfaceareas$subs_code == sc)] 
-      #calculates the surface area
+    int_function <- function(sc, vol, rfu, run) {
+      # row <- 1
+      # dat_row <- chla_df[row,]
+      # 
+      # sc <- dat_row$subs_code
+      # vol <- dat_row$vol_Etoh
+      # rfu <- dat_row$value_rfu
+      # run <- dat_row$run
       
-      run_vol <- ifelse(run_error == "none", run, run_error) 
-      vol_tile <- volcalc$vol_tile[which(volcalc$run == run_vol)] 
-      #calcuates these based on ru, note that fake moss, light gap and 2018 have special code
-      vol_other <- volcalc$vol_scrub[which(volcalc$run == run_vol)]
-      vol_eth <- as.numeric(ifelse(sc == T, vol_tile, vol_other)) 
-      
-      rfu_cor1 <- rfu - blanks$blanks[which(blanks$run == run)] 
+      sa <- sa_df$surface_area_m2[which(sa_df$subs_code == sc)] #calculates surface area
+      rfu_cor1 <- rfu - blank_df$blanks[which(blank_df$run == run)] 
       rfu_cor <- ifelse(rfu_cor1 < 0, 0, rfu_cor1)
-      #this is the blank code that calculates what to subtract by 
       
-      vol <- as.numeric(ifelse(is.na(vol_true), vol_eth, vol_true))#error catch for real vols
-
-      mg_m2 <- rfu_cor*masterslope*(1/1000)*(vol/1000)*(1/sa) #1000's for conversion factors 
-      
+      mg_m2 <- rfu_cor*slp*(1/1000)*(vol/1000)*(1/sa) #1000's for conversion factors 
       return(mg_m2)
+      
+    }
+    
+    
+  chla_df$value_mgm2 <-  mapply(int_function, chla_df$subs_code, chla_df$vol_Etoh,chla_df$value_rfu, chla_df$run)
+
+return(chla_df)
 }
-
-
+  
